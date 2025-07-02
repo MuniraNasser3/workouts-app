1b1f2c4d684f35bd11a42d50ee3d346155101275
Munira Nasser Abuhaimed <146862671+MuniraNasser3@users.noreply.github.com>
2025-07-02 04:37:46 -0700

    Push all recent changes: folder renames, Docker updates, and more

--- gymtrackr-frontend/src/pages/WorkoutList.tsx
+++ gymtrackr-frontend/src/pages/WorkoutList.tsx
@@ -1,5 +1,6 @@
 // src/pages/WorkoutList.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
